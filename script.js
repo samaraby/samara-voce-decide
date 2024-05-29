38fd792dbffc7895958aef30a306569c8c36e0ad
samaramonteirom <34330381-samaramonteirom@users.noreply.replit.com>
2024-05-29 23:46:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrinipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoresultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    enunciado: "pergunta 1",
+    alternativas:[
+      "Resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "pergunta 2",
+    alternativas:[
+      "Resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "pergunta 3",
+    alternativas:[
+      "Resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "pergunta 4",
+    alternativas:[
+      "Resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado: "pergunta 5",
+    alternativas:[
+      "Resposta 1",
+      "Resposta 2"
+    ]
+  }
+];
